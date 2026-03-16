a33b476b9025ba1c909a24995e2a1222826bb257
fei <fei@timesink>
2026-03-16 11:16:03 +0100

    install repo in new deployment

--- scripts/post.sh
+++ scripts/post.sh
@@ -2,6 +2,6 @@
 
 mkdir -p /var/tmp
 KVER="$(ls /usr/lib/modules)"
-dracut --no-hostonly --kver "$KVER" --reproducible \
+dracut --no-hostonly --kver "$KVER" --reproducible --add "repo" \
     -f "/usr/lib/modules/$KVER/initramfs.img"
 echo "Bye! :D"
