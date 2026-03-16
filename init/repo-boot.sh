e4739735d35c3929e94bc662739eef3df6673d67
fei <fei@timesink>
2026-03-16 21:01:53 +0100

    minor fixes

--- init/repo-boot.sh
+++ init/repo-boot.sh
@@ -5,7 +5,7 @@ type getarg >/dev/null 2>&1 || . /lib/dracut-lib.sh
 [ -z "$1" ] && printf "No sysroot specified! Exiting!\n" && exit 0
 sysroot="$1"
 
-ald_boot=$(getarg repo.boot)
+repo_boot=$(getarg repo.boot)
 [ -z "$repo_boot" ] && printf "No deployment specified! Exiting!\n" && exit 0
 
 mount -o remount,rw "$sysroot"
